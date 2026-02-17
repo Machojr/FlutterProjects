4db734572bf7d1ad19c83e1df34ebe00d7fcb61b
Machojr <abdulkarimmacho@gmail.com>
2026-02-17 11:48:55 +0300

    phase 1 authentication and authorization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'providers/auth_provider.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/signup_screen.dart';
 import 'utils/app_theme.dart';
 import 'utils/constants.dart';
-
-// TODO: After running `flutterfire configure`, uncomment this import:
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,19 +21,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        // Add providers here (AuthProvider, ChatProvider, etc.)
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
       ],
       child: MaterialApp(
         title: AppConstants.appName,
         theme: AppTheme.lightTheme,
         debugShowCheckedModeBanner: false,
-        initialRoute: AppConstants.loginRoute,
+        home: const _HomeRouter(),
         routes: {
-          AppConstants.loginRoute: (context) => const Scaffold(body: Center(child: Text('Login Screen Placeholder'))),
-          AppConstants.signupRoute: (context) => const Scaffold(body: Center(child: Text('Signup Screen Placeholder'))),
-          AppConstants.homeRoute: (context) => const Scaffold(body: Center(child: Text('Home Screen Placeholder'))),
+          AppConstants.loginRoute: (context) => const LoginScreen(),
+          AppConstants.signupRoute: (context) => const SignupScreen(),
+          AppConstants.homeRoute: (context) => const Scaffold(
+            body: Center(
+              child: Text('Home Screen Placeholder'),
+            ),
+          ),
         },
       ),
+    );
+  }
+}
+
+/// Router widget to handle navigation based on auth state
+class _HomeRouter extends StatelessWidget {
+  const _HomeRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthProvider>(
+      builder: (context, authProvider, _) {
+        // If user is logged in, go to home, otherwise go to login
+        if (authProvider.isUserLoggedIn) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Home Screen Placeholder'),
+            ),
+          );
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
